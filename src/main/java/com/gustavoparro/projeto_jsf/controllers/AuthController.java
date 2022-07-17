d4fba4386a86e0f5b856e1060fbb9d68f9cdbf94
Gustavo Ferreira Parro <gustavo.ferreira.parra@gmail.com>
2022-07-17 15:44:46 -0300

    Criando tela de Sign in e o controller correspondente.

---
+++ src/main/java/com/gustavoparro/projeto_jsf/controllers/AuthController.java
@@ -0,0 +1,22 @@
+package com.gustavoparro.projeto_jsf.controllers;
+
+import com.gustavoparro.projeto_jsf.models.AppUser;
+import jakarta.enterprise.inject.Model;
+
+@Model
+public class AuthController {
+
+    private AppUser appUser;
+
+    public void signIn() {
+        System.out.println(appUser);
+    }
+
+    public AppUser getAppUser() {
+        if (appUser == null) {
+            appUser = new AppUser();
+        }
+
+        return appUser;
+    }
+}
